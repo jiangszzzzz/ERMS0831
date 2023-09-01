b7de908f61c330253f304af7fd4e57e08531a8ac
jiangszzzzz <jiangsz92@163.com>
2023-09-01 09:19:44 +0800

    Initial commit

---
+++ app01/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Department(models.Model):
+    """ 部门表 """
+    title = models.CharField(verbose_name="标题", max_length=32)
+
+
+class User_info(models.Model):
+    """ 员工表 """
+    name = models.CharField(verbose_name="姓名", max_length=32)
+    password = models.CharField(verbose_name="", max_length=64)
+    age = models.IntegerField(verbose_name="年龄")
+    account = models.DecimalField(verbose_name="账户余额", max_digits=10, decimal_places=2, default=0)
+    create_time = models.DateTimeField(verbose_name="入职时间")
+
+    # 无约束
+    # depart_id = models.BigIntegerField(verbose_name="部门ID")
+
+    # 有约束 django 写 depart 生成  depart_id
+    # 有约束                    to 与那张表关联    to_field 表中的一列关联        级联删除
+    depart = models.ForeignKey(to="Department", to_field="id", on_delete=models.CASCADE)
+    # 有约束                    to 与那张表关联    to_field 表中的一列关联        置空
+    # depart = models.ForeignKey(id="Department", to_field="id", null=True, blank=True, on_delete=models.SET_NULL)
+
+    # django 代码中约束
+    gender_choices = ((1, "男"), (2, "女"),)
+    gender = models.SmallIntegerField(verbose_name="性别", choices=gender_choices)
---
+++ app01/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from app01 import models
+
+
+def depart_list(request):
+    """部门列表"""
+    # 去数据库中获得所有部门表
+    # [对象，对象]
+    queryset = models.Department.objects.all()
+
+    return render(request, 'depart_list.html', {'queryset': queryset})
+
+
+def depart_add(request):
+    """添加部门"""
+    if request.method == "GET":
+        return render(request, 'depart_add.html')
+
+    title = request.POST.get("title")
+    models.Department.objects.create(title=title)
+
+    return redirect("/depart/list/")
+
+
+def depart_delete(request):
+    """删除部门"""
+    # http://127.0.0.1:8000/depart/delete/?nid=1
+    nid = request.GET.get('nid')
+    models.Department.objects.filter(id=nid).delete()
+    return redirect("/depart/list/")
+
+
+def depart_edit(request, nid):
+    """修改部门"""
+    # http://127.0.0.1:8000/depart/1/edit/
+    if request.method == "GET":
+        row_object = models.Department.objects.filter(id=nid).first()
+        return render(request, "depart_edit.html", {"row_object": row_object})
+    title = request.POST.get("title")
+    models.Department.objects.filter(id=nid).update(title=title)
+    return redirect("/depart/list/")
+
